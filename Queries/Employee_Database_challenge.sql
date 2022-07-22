7fea43b2e1cb2506c87543b348edf26cabe278f5
ZakArmstrong99 <107213807+ZakArmstrong99@users.noreply.github.com>
2022-07-22 18:01:38 -0400

    Added complete analysis

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -10,13 +10,13 @@ WHERE _______
 ORDER BY _____, _____ DESC;
 
 -- Retirement titles
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
+SELECT e.emp_no,
 e.first_name,
 e.last_name,
 ti.title,
 ti.from_date,
 ti.to_date
---INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
@@ -38,7 +38,9 @@ WHERE (rt.to_date = '9999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
 
 
-SELECT * FROM unique_titles;
+SELECT * 
+FROM unique_titles
+;
 
 --Retiring titles
 SELECT COUNT (ut.title), ut.title
@@ -49,6 +51,8 @@ ORDER BY COUNT(ut.title) DESC;
 
 SELECT * FROM retiring_titles;
 
+SELECT SUM (rt.count) AS total_retiring
+FROM retiring_titles AS rt;
 
 
 --Mentorship eligibility
@@ -66,7 +70,24 @@ ON (e.emp_no = de.emp_no)
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
+ORDER BY e.emp_no, ti.title;
 
 
 SELECT * FROM mentorship_eligibility;
+
+SELECT COUNT (me.emp_no) AS total_mentors
+FROM mentorship_eligibility AS me;
+
+SELECT * 
+FROM titles
+WHERE emp_no = '10291';
+
+SELECT * 
+FROM dept_emp
+WHERE emp_no = '10095'
+;
+
+DROP TABLE retirement_titles ;
+DROP TABLE unique_titles ;
+DROP TABLE retiring_titles ;
+DROP TABLE mentorship_eligibility;
